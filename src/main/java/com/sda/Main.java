9757a129036ffb6162994c12bdc5e787d7bf3bb8
Victor Corobceanu <victor.corobceanu@yahoo.ro>
2019-02-16 11:01:23 +0200

    gitignore

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -8,14 +8,14 @@ import com.sda.model.User;
 public class Main {
 
     public static void main(String[] args) {
-//        User user = new User("admin", "admin");
-//        UserDao userDao = new UserDao();
-//        userDao.createEntity(user);
-//        Employee employee = new Employee();
-//        employee.setName("Cosmin C");
+        User user = new User("admin", "admin");
+        UserDao userDao = new UserDao();
+        userDao.createEntity(user);
+        Employee employee = new Employee();
+        employee.setName("Cosmin C");
           EmployeeDao employeeDao = new EmployeeDao();
-          Employee employee = employeeDao.getEntityById(Employee.class, 1L);
+//          Employee employee = employeeDao.getEntityById(Employee.class, 1L);
         System.out.println(employee.getName());
-//        employeeDao.createEntity(employee);
+        employeeDao.createEntity(employee);
     }
 }
